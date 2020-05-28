eb0e2936ba071ec1aeea43037380e8e2fd1e783e
muhamet kaqandolli <muhamet_kaqandolli@hotmail.com>
2020-05-28 16:10:48 +0200

    Add project files.

---
+++ Ushtrime/HealthProfile.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ushtrime
+{
+    class HealthProfile
+    {
+        private int _patientYearOfBirth;
+        private string _patientGender;
+
+        public HealthProfile(string fname, string lname, int yob, string gender, int height, int weight)
+        {
+            PatientFirstName = fname;
+            PatientLastName = lname;
+            PatientYearOfBirth = yob;
+            PatientGender = gender;
+            PatientHeight = height;
+            PatientWeight = weight;
+        }
+
+        public string PatientFirstName { get; set; }
+        public string PatientLastName { get; set; }
+        public int PatientHeight { get; set; }
+        public int PatientWeight { get; set; }
+
+        public int PatientYearOfBirth
+        {
+            get { return _patientYearOfBirth; }
+            set
+            {
+                while (value < 1900 || value > 2016)
+                {
+                    Console.WriteLine("Invalid year of birth. Please re-enter: ");
+                    value = Convert.ToInt32(Console.ReadLine());
+                }
+
+                _patientYearOfBirth = value;
+            }
+        }
+
+        public string PatientGender
+        {
+            get { return _patientGender; }
+            set
+            {
+                while (value.ToUpper() != "M" && value.ToUpper() != "F")
+                {
+                    Console.WriteLine("Please enter valid gender (M or F)");
+                    value = Console.ReadLine();
+                }
+
+                _patientGender = value;
+            }
+        }
+
+        public int Age()
+        {
+            return DateTime.Today.Year - PatientYearOfBirth;
+        }
+
+        public int MaxHeartRate()
+        {
+            return 220 - Age();
+        }
+
+        public int MaxTargetRate()
+        { 
+            return (Convert.ToInt32(MaxHeartRate()* 0.85));
+        }
+
+        public int MinTargetRate()
+        { 
+            return (Convert.ToInt32(MaxHeartRate()* 0.5));
+        }
+
+        public double BMI()
+        {
+            return (PatientWeight / (Math.Pow(PatientHeight, 2))) * 703;
+        }
+
+        public string BmiEvaluation()
+        {
+            if (BMI() < 18.5)
+                return "Underweight";
+            else if (BMI() < 25)
+                return "Normal";
+            else if (BMI() < 30)
+                return "Overweight";
+            else
+                return "Obese";
+            
+        }
+    }
+}
---
+++ Ushtrime/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ushtrime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your first name? ");
+            string fname = Console.ReadLine();
+            Console.Write("What is your last name? ");
+            string lname = Console.ReadLine();
+            Console.Write("What is your year of birth? ");
+            int yob = Convert.ToInt32(Console.ReadLine());
+            Console.Write("What is your gender? ('M' or 'F') ");
+            string gender = Console.ReadLine();
+            Console.Write("What is your height in inches? ");
+            int height = Convert.ToInt32(Console.ReadLine());
+            Console.Write("What is your weight in pounds? ");
+            int weight = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine();
+            HealthProfile patient1 = new HealthProfile(fname, lname, yob, gender, height, weight);
+
+            Console.WriteLine("Name: {0} {1}", patient1.PatientFirstName, patient1.PatientLastName );
+            Console.WriteLine("Year Of Birth: {0}", patient1.PatientYearOfBirth);
+            Console.WriteLine("Age: {0}", patient1.Age());
+            Console.WriteLine("Gender: {0}", patient1.PatientGender.ToUpper() == "M" ? "Male" : "Female");
+            Console.WriteLine("Max safe heart rate: {0}. \n Workout heart rate target range {1} to {2}",
+                patient1.MaxHeartRate(), patient1.MinTargetRate(), patient1.MaxTargetRate());
+            Console.WriteLine("Height: {0}\". Weight {1} lbs.", patient1.PatientHeight, patient1.PatientWeight);
+            Console.WriteLine("BMI: {0:##:##}%, which means you are {1}", patient1.BMI(), patient1.BmiEvaluation());
+
+
+            Console.ReadLine();
+        }
+    }
+}
